c091ce850f67f119d0ff3e98e42c1ab4fcb27a64
AriannaToitonova <ariannatoitonova@gmail.com>
2023-05-26 17:11:24 +0600

    dz4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class RegistrationForm extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: " ",
+            phoneNumber: " ",
+            email: " ",
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+
+        handleChange(e){
+            this.setState({
+                [e.target.name]: e.target.value
+            });
+        };
+
+        handleSubmit(e){
+            console.log(this.state)
+            e.preventDefault();
+        };
+
+
+    render() {
+        return (
+            <form className="form" onSubmit={this.handleSubmit}>
+                <h2>Регистрация пользователя</h2>
+                <div className="form-group">
+                    <label>ФИО:</label>
+                    <input
+                        type="text"
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Номер телефона:</label>
+                    <input
+                        type="tel"
+                        name="phoneNumber"
+                        value={this.state.phoneNumber}
+                        onChange={this.handleChange}
+
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Email адрес:</label>
+                    <input
+                        type="email"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <input type="submit" value="Submit"/>
+            </form>
+        );
+    }
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
